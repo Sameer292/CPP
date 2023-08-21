d8b420cf4a8e93aac110817a3ee303e656094649
Sameer Paudel <83913671+Sameer292@users.noreply.github.com>
2023-08-21 22:27:37 +0545

    Add files via upload
    Day-2

---
+++ Day-2/data_types.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //there are a lot of variables to use in c++
+    // int, unsigned int, long, unsigned long, long long, unsigned long long, float, double, char, boolean etc are the data types.. We'll some of them
+
+    int i=10; //int can store only integer values
+    float f=10.10; //float can take integers as well as fractional values
+    char c='f'; //char takes characher values and the value should be assigned using the '' operator
+    bool b=true; //bool take either true or false only.
+
+    cout <<i<<"\n"<<f<<"\n"<<c<<"\n"<<b<<"\n";
+    return 0;
+}
+
